e81c3e5b597900233c1ec4ece8bbf05b00ef9d4d
KLiuCSUN <kevin.liu.149@my.csun.edu>
2017-10-10 23:33:49 -0400

    ControllerSetup

--- src/main/java/rushi/controllers/MainController.java
+++ src/main/java/rushi/controllers/MainController.java
@@ -1,5 +1,28 @@
 package rushi.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class MainController {
 
+	public MainController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	//Returns list of all chips.
+	@RequestMapping(value="/", method=RequestMethod.GET, produces = "application/json")
+	public String goToLogin() {
+		String responseJSON = "";
+		try {
+			
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return responseJSON;
+	} 
 }
